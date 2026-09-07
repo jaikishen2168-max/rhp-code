2d9092ee47bd88ae04a6deecc49505a2e6cce243
JAI KISHEN <jaikishen2168@gmail.com>
2026-09-07 13:33:38 +0530

    Number of Provinces.java

--- Number of Provinces.java
+++ Number of Provinces.java
@@ -1,32 +1,31 @@
 class Solution {
-     private int find(int [] ldr,int node){
-        if(ldr[node]!=node){
-            ldr[node]=find(ldr,ldr[node]);
+    private int find(int ldr[], int node){
+        if(ldr[node] != node){
+            ldr[node] = find(ldr, ldr[node]);
         }
         return ldr[node];
     }
-    private void join(int ldr[],int lt,int rt){
-        ldr[find(ldr,rt)]=find(ldr,lt);
+    private void join(int ldr[], int lt, int rt){
+        ldr[find(ldr,rt)] = find(ldr,lt);
     }
-
     public int findCircleNum(int[][] g) {
-
-        int n = g.length;
-        int[] ldr = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            ldr[i] = i;
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-                if (g[i][j] == 1) {
-                    join(ldr, i, j);
+        int N = g.length;
+        int ldr[] = new int[N+1];
+        for(int node = 1; node <= N; node++) ldr[node] = node;
+        for(int col=1; col<=N; col++){
+            for(int row=1; row<=N; row++){
+                if (row == col) continue;
+                if(g[row-1][col-1] == 1){
+                    join(ldr,row,col);
                 }
             }
         }
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (find(ldr, i) == i) {
-                cou
+        int provinces = 0;
+        for (int node = 1; node <= N; node++){
+            if(ldr[node] == node){
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+};
